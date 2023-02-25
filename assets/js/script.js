b6f03ca4db2a5363d2c7de2cbf8f0eccf7f7c21d
thxrhmn <rahmanmuhaemin@gmail.com>
2023-02-25 11:30:30 +0700

    tidy uppp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,41 @@
 const getData = () => {
-    // ambil value/hasil inputan dari id
-    let name = document.getElementById("name").value
-    let email = document.getElementById("email").value
-    let phone = document.getElementById("phone").value
-    let subject = document.getElementById("subject").value
-    let message = document.getElementById("message").value
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
 
-    // cek kondisi jika variable isinya kosong
-    // maka tampilkan alert 
     if (name == "") {
-        return alert("Nama belum diisi")
+        alert("Nama Cannot be empty");
+        return;
     } else if (email == "") {
-        return alert("Email belum diisi")
+        alert("Email Cannot be empty");
+        return;
     } else if (phone == "") {
-        return alert("Phone belum diisi")
+        alert("Phone Cannot be empty");
+        return;
     } else if (subject == "") {
-        return alert("Subject belum diisi")
+        alert("Subject Cannot be empty");
+        return;
     } else if (message == "") {
-        return alert("Message belum diisi")
-    } 
-    
-    // email penerima
-    const emailReceiver = "rahmanmuhaemin@gmail.com"
+        alert("Message Cannot be empty");
+        return;
+    }    
 
-    // buat element a
-    let a = document.createElement('a')
+    const emailReceiver = "rahmanmuhaemin@gmail.com";
+    const bodyMessage = `hello my name is ${name}, ${subject} with me asap you can call me here (+${phone}) \n\n${message}\n\n`;
 
-    // format mailto
-    a.href = `mailto:${emailReceiver}?subject=${subject}&body=hello my name is ${name}, ${subject} with me asap you can call me here (+${phone}) %0D%0A%0D%0A${message}%0D%0A%0D%0A`
+    let a = document.createElement("a");
+    a.href = `mailto:${emailReceiver}?subject=${subject}&body=${encodeURIComponent(bodyMessage)}`;
+    a.click();
 
-    // click function bawaaan js || ketika di klik 
-    // otomatis membuka email
-    a.click()
-
-    // object 
     let data = {
         name,
         email,
         phone,
         subject,
-        message
-    }
+        message,
+    };
 
-    // menampilkan pada console
-    // console.log(`${name}\n${email}\n${phone}\n${subject}\n${message}`)
-    console.log(data)
-}
+    console.log(data);
+};
